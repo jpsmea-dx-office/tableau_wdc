0f0afd84b2755dfa84041016c3e6add93df6e865
中小企業庁DXオフィス <49966557+jpsmea-dx-office@users.noreply.github.com>
2020-05-26 17:58:04 +0900

    Add files via upload

--- resas_wdc_estate/resas_wdc_estate.js
+++ resas_wdc_estate/resas_wdc_estate.js
@@ -4,54 +4,59 @@
     var resas_api_key = "4KXUy7Q9cGCp0aYFDCaPOiHE4jfFkW8cAuVfGqYi"
 
     myConnector.getSchema = function (schemaCallback) {
-    var cols = [
-        { id : "prefCode", dataType : tableau.dataTypeEnum.string },
-        { id : "prefName", dataType : tableau.dataTypeEnum.string },
-        { id : "cityCode", dataType : tableau.dataTypeEnum.string },
-        { id : "cityName", dataType : tableau.dataTypeEnum.string },
-        { id : "displayType", dataType : tableau.dataTypeEnum.string },
-        { id : "year", dataType : tableau.dataTypeEnum.int },
-        { id : "value", dataType : tableau.dataTypeEnum.int}
-    ];
+        var cols = [
+            { id : "prefCode", dataType : tableau.dataTypeEnum.string },
+            { id : "prefName", dataType : tableau.dataTypeEnum.string },
+            { id : "cityCode", dataType : tableau.dataTypeEnum.string },
+            { id : "cityName", dataType : tableau.dataTypeEnum.string },
+            { id : "displayType", dataType : tableau.dataTypeEnum.string },
+            { id : "year", dataType : tableau.dataTypeEnum.int },
+            { id : "value", dataType : tableau.dataTypeEnum.int}
+        ];
 
-    var tableInfo = {
-        id : "Estate",
-        alias : "不動産取引価格",
-        columns : cols
+        var tableInfo = {
+            id : "Estate",
+            alias : "不動産取引価格",
+            columns : cols
+        };
+
+        schemaCallback([tableInfo]);
     };
 
-    schemaCallback([tableInfo]);
-};
-
     myConnector.getData = function(table, doneCallback) {
-    $.ajax({ 
-        url: "https://opendata.resas-portal.go.jp/api/v1/townPlanning/estateTransaction/bar?year=2017&prefCode=13&cityCode=-&displayType=2",
-        dataType: 'json',
-        type: "GET",
-        headers: {
-            'X-API-KEY': resas_api_key
-        }, 
-        async: "false" ,
-        success: function(resp) {
-            var tableData = []; 
+    
+        var param_years = [2009,2010,2011,2012,2013,2014,2015,2016,2017];
 
-           for ( i = 0; i < resp.result.years.length; i++ ) {
-                tableData.push({
-                "prefCode": resp.result.prefCode,
-                "prefName": resp.result.prefName,
-                "cityCode": resp.result.cityCode,
-                "cityName": resp.result.cityName,
-                "displayType": "土地(商業地)",
-                "year": resp.result.years[i].year,
-                "value": resp.result.years[i].value
+        param_years.each(function(param_year){
+            $.ajax({ 
+                url: "https://opendata.resas-portal.go.jp/api/v1/townPlanning/estateTransaction/bar?year="&param_year&"&prefCode=13&cityCode=-&displayType=2",
+                dataType: 'json',
+                type: "GET",
+                headers: {
+                    'X-API-KEY': resas_api_key
+                }, 
+                async: "false" ,
+                success: function(resp) {
+                    var tableData = []; 
+
+                    for ( i = 0; i < resp.result.years.length; i++ ) {
+                        tableData.push({
+                        "prefCode": resp.result.prefCode,
+                        "prefName": resp.result.prefName,
+                        "cityCode": resp.result.cityCode,
+                        "cityName": resp.result.cityName,
+                        "displayType": "土地(商業地)",
+                        "year": resp.result.years[i].year,
+                        "value": resp.result.years[i].value
+                        });
+                    }
+
+                    table.appendRows(tableData);
+                },
             });
-        }
-
-        table.appendRows(tableData);
-        doneCallback();
-        },
-    });
-};
+        });
+        doneCallback();    
+    };
 
     tableau.registerConnector(myConnector);
 })();
